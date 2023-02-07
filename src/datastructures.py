ebfaaeb745574a88777c3d58825b36a7b001c4cc
beaesreal <beaesreal@gmail.com>
2023-02-07 19:06:20 +0000

    Exercise finished and tests passed 🎉

--- src/datastructures.py
+++ src/datastructures.py
@@ -10,49 +10,36 @@ update this file to implement the following already declared methods:
 from random import randint
 
 
-
 class FamilyStructure:
-    def __init__(self, last_name):
+    def __init__(self, last_name, members):
+        
         self.last_name = last_name
-
-        # Example list of members
-        self._members = []
-
+    # Example list of members
+        self._members = members
 
     # Read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
-        
 
     def add_member(self, member):
         # Fill this method and update the return
-        member["lastname"] = self.last_name
-        if "id" not in member : member["id"] = self._generateId()
-        self._members.append(member)
-
-        return member
-
+        pass
 
     def delete_member(self, id):
         # Fill this method and update the return
-        for i in range(len(self._members)):
-            if self._members[i]['id'] == id:
-                aux = self._members[i]
-                del self._members[i]
-                return aux
-
-        return {"msg" : "Id not found"}
-
+        for person in self._members:
+            if id == person['id']:
+                self._members.remove(person)
+                return True
+        return None
 
     def get_member(self, id):
-        for i in range(len(self._members)):
-            if self._members[i]['id'] == id:
-                return self._members[i]
-
-        return {"msg" : "Id not found"}
+        # Fill this method and update the return
+        for person in self._members:
+            if id == person['id']:
+                return person
+        return None
         
-        
-
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
